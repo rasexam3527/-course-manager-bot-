ef09975820538dd6791cc1424aa4d588ffa17066
rasexam3527 <abdulqadiraliraj@gmail.com>
2026-08-05 10:00:55 +0530

    Add configuration file for environment variables

---
+++ config.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+DATABASE_NAME = "bot.db"
+
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "0"))
